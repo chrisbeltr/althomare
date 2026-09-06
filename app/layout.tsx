03070e3629baa77a3d05ead6ec763404d010286c
chrisbeltr <christianbeltran@uchicago.edu>
2026-09-05 21:11:50 -0500

    changed layout for main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-200 text-gray-900 min-h-dvh flex flex-col justify-between">
+      <body className="bg-gray-200 text-gray-900 min-h-dvh flex flex-col justify-between items-center">
         {children}
       </body>
     </html>
