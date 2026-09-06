cf212b1ba661a2747e25e19522e5d689c54e5b85
chrisbeltr <christianbeltran@uchicago.edu>
2026-09-05 21:16:06 -0500

    items stay in center when smaller

--- app/template.tsx
+++ app/template.tsx
@@ -4,7 +4,9 @@ export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className="relative h-full flex-1">{children}</main>
+      <main className="relative h-full flex-1 flex flex-col items-center">
+        {children}
+      </main>
     </>
   );
 }
