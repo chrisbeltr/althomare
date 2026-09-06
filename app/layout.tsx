5250605f3e2664647963308ee4fd08352b1b3c84
chrisbeltr <christianbeltran@uchicago.edu>
2026-09-05 21:16:06 -0500

    items stay in center when smaller

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-200 text-gray-900 min-h-dvh flex flex-col justify-between items-center">
+      <body className="bg-gray-200 text-gray-900 min-h-dvh flex flex-col justify-between">
         {children}
       </body>
     </html>
