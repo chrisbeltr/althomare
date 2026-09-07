3522dbff7e65feed3afbcd2b91261aa1a68cc13f
chrisbeltr <christianbeltran@uchicago.edu>
2026-09-07 15:17:33 -0500

    satisfied with picture so far

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import Image from "next/image";
 export default function Page() {
   return (
     <>
-      <Image src="/group.jpeg" alt="Group picture of Althomare company" fill />
+      <div className="relative max-w-275 w-full">
+        {/* <div className="max-w-225"> */}
+        <Image
+          src="/group.jpeg"
+          alt="Group picture of Althomare company"
+          width={2048}
+          height={1536}
+          loading="eager"
+        />
+        {/* </div> */}
+        <div className="absolute right-0 top-5 p-8 text-5xl font-bold text-white rounded-md">
+          Where the magic happens.
+        </div>
+      </div>
     </>
   );
 }
