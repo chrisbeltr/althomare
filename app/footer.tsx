022f935b5396887a621df5ba846b5b1096db2ad3
chrisbeltr <christianbeltran@uchicago.edu>
2026-09-10 16:33:39 -0500

    better tags

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,15 +1,15 @@
 export default function Footer() {
   return (
-    <div className="w-full p-8 flex justify-center bg-red-950 text-white">
+    <footer className="w-full p-8 flex justify-center bg-red-950 text-white">
       <div className="max-w-250 w-full flex justify-between items-center flex-wrap gap-x-8 gap-y-2">
-        <div className="text-2xl shrink-0">
+        <h2 className="text-2xl shrink-0">
           © Althomare Theater {new Date().getFullYear()}
-        </div>
-        <div className="shrink-0">
+        </h2>
+        <span className="shrink-0 text-sm">
           Reach out at{" "}
           <a href="mailto:contact@althomare.com">contact@althomare.com</a>
-        </div>
+        </span>
       </div>
-    </div>
+    </footer>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,15 @@ export default function Page() {
           height={1536}
           loading="eager"
         />
-        <div className="absolute right-0 top-0 md:top-5 p-5 md:p-8 text-2xl sm:text-3xl md:text-5xl font-bold text-white">
+        <h2 className="absolute right-0 top-0 md:top-5 p-5 md:p-8 text-2xl sm:text-3xl md:text-5xl font-bold text-white">
           Where the magic happens.
-        </div>
+        </h2>
       </div>
       <div className="relative w-full flex flex-col items-center bg-gray-300 text-gray-600">
         <div className="relative max-w-275 w-full p-5 flex flex-col md:flex-row justify-end">
           <div className="md:absolute md:left-5 md:h-[calc(100%-40px)] md:w-3/4 upnext-gradient md:flex md:flex-col md:justify-center">
-            <div className="font-bold text-2xl">Coming up next...</div>
-            <div className="md:w-2/3 text-xl pb-5 md:p-0">
+            <h2 className="font-bold text-2xl">Coming up next...</h2>
+            <p className="md:w-2/3 text-xl pb-5 md:p-0">
               Our next production will be Little Shop of Horrors! No, we will
               not have a giant carnivorous plant shipped to Althea's apartment.
               Or dentistry equipment. Sorry Pirelli. Make sure to fill out the
@@ -30,7 +30,7 @@ export default function Page() {
               Additionally, we will be hosting our first ever Althonys award
               ceremony, so make sure to cast your votes in that same Google
               Form!
-            </div>
+            </p>
           </div>
           <Image
             src="/littleshop.webp"
